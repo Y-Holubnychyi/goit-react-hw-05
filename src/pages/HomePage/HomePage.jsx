c9924bfd1d2d91e36776d3d2b684ef39a808e500
Eugene <y.holubnychyi@gmail.com>
2025-04-21 21:43:46 -0230

    minor fix2

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { fetchTrendingMovies } from "../../Api/Api";
+import { fetchTrendingMovies } from "../../Ap/Ap";
 import MovieList from "../../components/MovieList/MovieList";
 import Loader from "../../components/Loader/Loader";
 import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
